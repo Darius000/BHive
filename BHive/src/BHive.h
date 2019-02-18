9b6f38611223111d41f331b39529877a338673ef
Darius Bigbee <dariusbigbee12@gmail.com>
2019-02-17 20:14:46 -0500

    Added Mouse and Key Codes

--- BHive/src/BHive.h
+++ BHive/src/BHive.h
@@ -4,8 +4,13 @@
 
 
 #include "BHive/Application.h"
+#include "BHive/Input.h"
+#include "BHive/KeyCodes.h"
+#include "BHive/MouseCodes.h"
 #include "BHive/Layer.h"
 #include "BHive/Log.h"
+
+//---Layers------->
 #include "BHive/ImGui/ImGuiLayer.h"
 #include "BHive/Rendering/RenderLayer.h"
 
---
+++ BHive/src/BHive/KeyCodes.h
@@ -0,0 +1,126 @@
+#pragma once
+
+//From glfw3.h
+
+#define BH_KEY_SPACE              32
+#define BH_KEY_APOSTROPHE         39  /* ' */
+#define BH_KEY_COMMA              44  /* , */
+#define BH_KEY_MINUS              45  /* - */
+#define BH_KEY_PERIOD             46  /* . */
+#define BH_KEY_SLASH              47  /* / */
+#define BH_KEY_0                  48
+#define BH_KEY_1                  49
+#define BH_KEY_2                  50
+#define BH_KEY_3                  51
+#define BH_KEY_4                  52
+#define BH_KEY_5                  53
+#define BH_KEY_6                  54
+#define BH_KEY_7                  55
+#define BH_KEY_8                  56
+#define BH_KEY_9                  57
+#define BH_KEY_SEMICOLON          59  /* ; */
+#define BH_KEY_EQUAL              61  /* = */
+#define BH_KEY_A                  65
+#define BH_KEY_B                  66
+#define BH_KEY_C                  67
+#define BH_KEY_D                  68
+#define BH_KEY_E                  69
+#define BH_KEY_F                  70
+#define BH_KEY_G                  71
+#define BH_KEY_H                  72
+#define BH_KEY_I                  73
+#define BH_KEY_J                  74
+#define BH_KEY_K                  75
+#define BH_KEY_L                  76
+#define BH_KEY_M                  77
+#define BH_KEY_N                  78
+#define BH_KEY_O                  79
+#define BH_KEY_P                  80
+#define BH_KEY_Q                  81
+#define BH_KEY_R                  82
+#define BH_KEY_S                  83
+#define BH_KEY_T                  84
+#define BH_KEY_U                  85
+#define BH_KEY_V                  86
+#define BH_KEY_W                  87
+#define BH_KEY_X                  88
+#define BH_KEY_Y                  89
+#define BH_KEY_Z                  90
+#define BH_KEY_LEFT_BRACKET       91  /* [ */
+#define BH_KEY_BACKSLASH          92  /* \ */
+#define BH_KEY_RIGHT_BRACKET      93  /* ] */
+#define BH_KEY_GRAVE_ACCENT       96  /* ` */
+#define BH_KEY_WORLD_1            161 /* non-US #1 */
+#define BH_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define BH_KEY_ESCAPE             256
+#define BH_KEY_ENTER              257
+#define BH_KEY_TAB                258
+#define BH_KEY_BACKSPACE          259
+#define BH_KEY_INSERT             260
+#define BH_KEY_DELETE             261
+#define BH_KEY_RIGHT              262
+#define BH_KEY_LEFT               263
+#define BH_KEY_DOWN               264
+#define BH_KEY_UP                 265
+#define BH_KEY_PAGE_UP            266
+#define BH_KEY_PAGE_DOWN          267
+#define BH_KEY_HOME               268
+#define BH_KEY_END                269
+#define BH_KEY_CAPS_LOCK          280
+#define BH_KEY_SCROLL_LOCK        281
+#define BH_KEY_NUM_LOCK           282
+#define BH_KEY_PRINT_SCREEN       283
+#define BH_KEY_PAUSE              284
+#define BH_KEY_F1                 290
+#define BH_KEY_F2                 291
+#define BH_KEY_F3                 292
+#define BH_KEY_F4                 293
+#define BH_KEY_F5                 294
+#define BH_KEY_F6                 295
+#define BH_KEY_F7                 296
+#define BH_KEY_F8                 297
+#define BH_KEY_F9                 298
+#define BH_KEY_F10                299
+#define BH_KEY_F11                300
+#define BH_KEY_F12                301
+#define BH_KEY_F13                302
+#define BH_KEY_F14                303
+#define BH_KEY_F15                304
+#define BH_KEY_F16                305
+#define BH_KEY_F17                306
+#define BH_KEY_F18                307
+#define BH_KEY_F19                308
+#define BH_KEY_F20                309
+#define BH_KEY_F21                310
+#define BH_KEY_F22                311
+#define BH_KEY_F23                312
+#define BH_KEY_F24                313
+#define BH_KEY_F25                314
+#define BH_KEY_KP_0               320
+#define BH_KEY_KP_1               321
+#define BH_KEY_KP_2               322
+#define BH_KEY_KP_3               323
+#define BH_KEY_KP_4               324
+#define BH_KEY_KP_5               325
+#define BH_KEY_KP_6               326
+#define BH_KEY_KP_7               327
+#define BH_KEY_KP_8               328
+#define BH_KEY_KP_9               329
+#define BH_KEY_KP_DECIMAL         330
+#define BH_KEY_KP_DIVIDE          331
+#define BH_KEY_KP_MULTIPLY        332
+#define BH_KEY_KP_SUBTRACT        333
+#define BH_KEY_KP_ADD             334
+#define BH_KEY_KP_ENTER           335
+#define BH_KEY_KP_EQUAL           336
+#define BH_KEY_LEFT_SHIFT         340
+#define BH_KEY_LEFT_CONTROL       341
+#define BH_KEY_LEFT_ALT           342
+#define BH_KEY_LEFT_SUPER         343
+#define BH_KEY_RIGHT_SHIFT        344
+#define BH_KEY_RIGHT_CONTROL      345
+#define BH_KEY_RIGHT_ALT          346
+#define BH_KEY_RIGHT_SUPER        347
+#define BH_KEY_MENU               348
